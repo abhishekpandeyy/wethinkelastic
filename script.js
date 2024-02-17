4fefca0a77888c9a1ddf66b4ccbdb566fabd30c3
Abhishek Pandey <abhishekpandey8520@gmail.com>
2024-02-18 02:09:32 +0530

    80%

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+gsap.to('.scrollerPage h1', {
+    scrollTrigger: {
+        trigger: '.scrollerPage',
+        pin: true,
+        scroller:'body',
+        scrub: 1,
+        start: 'top 10%',
+        end: 'bottom -40%',
+        // markers: true,
+    },
+    // ease: "slow",
+    transform: `translateX(-70%)`,
+})
+gsap.to('.vidST', {
+    scrollTrigger: {
+        trigger: '.first',
+        // pin: true,
+        scroller:'body',
+        scrub: 5,
+        start: 'top -30%',
+        end: 'bottom -25%',
+        // markers: true,
+    },
+    // duration:2,
+    width: '100%',
+    // height: 'auto',
+})
+
+
