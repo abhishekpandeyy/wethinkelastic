5c220eb5dbd9f20d3ecda05ed8513f16afe8435e
Abhishek Pandey <abhishekpandey8520@gmail.com>
2024-03-07 01:16:41 +0530

    mouse-follower updated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ gsap.to('.scrollerPage h1', {
     scrollTrigger: {
         trigger: '.scrollerPage',
         pin: true,
-        scroller:'body',
+        scroller: 'body',
         scrub: 1,
         start: 'top 10%',
         end: 'bottom -40%',
@@ -17,21 +17,63 @@ gsap.to('.vidST', {
     scrollTrigger: {
         trigger: '.first',
         // pin: true,
-        scroller:'body',
+        scroller: 'body',
         scrub: 5,
         start: 'top -30%',
         end: 'bottom -25%',
-        // markers: true,
+        
     },
     // duration:2,
     width: '100%',
     // height: 'auto',
 })
 
+var cursor = document.querySelector('.followerOfImg');
 
-// var importImg=document.querySelectorAll('.import');
-
-// importImg.forEach(function(elem){
+var imgg=document.querySelectorAll('.divs');
 
 
-// })
+document.addEventListener('mousemove',function(details){
+    // console.log(details.target.classList.contains('divs'));
+    gsap.to('#followerOfImg',{
+        top:details.y,
+        left:details.x,
+        
+    })
+})
+
+
+imgg.forEach(function(elem)
+{
+    elem.addEventListener('mouseenter',function(){
+        console.log("enter");
+        gsap.to('#followerOfImg',{
+            transform: 'translate(-50%,-50%) scale(1)',
+            opacity:1,
+            delay:.2,
+            duration:.6,
+        })
+    })
+    elem.addEventListener('mouseout',function(){
+        console.log("leave");
+        gsap.to('#followerOfImg',{
+            transform: 'translate(-50%,-50%) scale(0)',
+            opacity:0,
+            delay:0,
+        })
+       
+    })
+})
+
+gsap.to('#rototingImg',{
+    rotate:360,
+    duration:13,
+    ease:'none',
+    repeat:-1,
+
+})
+
+Shery.textAnimate('#hello',{
+    y:80,
+    duration:4,
+})
